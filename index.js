e6e948e40db23c27de519b7eef48002e8b54eeb7
LeFizzy <honterparade@gmail.com>
2019-03-20 20:54:04 +0100

    Add code generation method.

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Open-Promotion-API method import.
+ */
+
+module.exports = {
+    generate: require('./lib/generate-promos')
+};
---
+++ test/test.js
@@ -0,0 +1,9 @@
+const openPromotion = require('../index');
+
+describe('Promotion code generation test: ', () => {
+    it('Should generate 10 strings of length 7: ', (done) => {
+        let promoCodeArr = openPromotion.generate( 10, 7);
+
+        if (promoCodeArr.length == 10) done();
+    });
+});
