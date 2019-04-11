a93281d57c9cd5c366e82ea7e77d8d3db492a78c
LeFizzy <honterparade@gmail.com>
2019-04-11 16:49:58 +0200

    Small change in the test file.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const openPromotion = require('../index');
 
 describe('Promotion code generation test: ', () => {
-    it('Should generate 10 strings of length 7: ', (done) => {
+    it('Should generate 10 strings of length 7.', (done) => {
         let promoCodeArr = openPromotion.generate( 10, 7);
 
         if (promoCodeArr.length == 10) done();
